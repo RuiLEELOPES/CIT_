67fde4348f7dc3a485a92fdf8fdb514de9e41151
RuiLEELOPES <64294651+RuiLEELOPES@users.noreply.github.com>
2020-05-09 11:03:12 +0900

    0.0.3

---
+++ 2000509/a.py
@@ -0,0 +1,12 @@
+a = 
+b = 2
+c = False
+
+while (b < a) and (not c):
+    if a % b == 0:
+        print(b)
+        print("O")
+        c = True
+    elif b == a -1:
+        print("X")
+    b = b+1
--- 2000509/print.py
+++ 2000509/print.py
@@ -1,1 +1,8 @@
-print("a")
+a = 711
+b = 1
+while(b<a):
+    if a%b == 0:
+        print("X")
+    else:
+        print("O")
+    b = b + 1
