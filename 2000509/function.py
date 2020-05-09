1c657576b26f106ca98eec150f266f2b966f3114
RuiLEELOPES <64294651+RuiLEELOPES@users.noreply.github.com>
2020-05-09 11:31:48 +0900

    0.0.4

---
+++ 2000509/function.py
@@ -0,0 +1,16 @@
+# # def add(num):
+#    resuH = num+5
+#    return resuH
+# #
+# #    print(add5(3))
+# def mult6(num):
+#     resuH = num*6
+#     return resuH
+#
+#
+# print(mult6(add(3)))
+def add(a,b):
+    return a+b
+    print("a")
+
+print(add(3,9))
